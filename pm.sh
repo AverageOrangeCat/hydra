f3bd38df26d7944de38bbf2896666a99622de6f8
AverageOrangeCat <74761380+AverageOrangeCat@users.noreply.github.com>
2024-11-04 10:01:07 +0100

    Add basic interaction menu

--- pm.sh
+++ pm.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 VERSION=0.0.2-test
+
+if [ $# -ge 1 ]; then
+    if [ $1 = version ]; then
+        echo $VERSION
+        exit 0
+    fi
+fi
+
+cat << EOF
+========================<[ COMMANDS ]>========================
+- help > Creates this commands menu
+EOF
