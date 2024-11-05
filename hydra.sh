08e37607c176e10ee2091a7756d334beb90965d7
AverageOrangeCat <74761380+AverageOrangeCat@users.noreply.github.com>
2024-11-05 14:03:19 +0100

    Removed script separation + renamed reaource folder properly

--- hydra.sh
+++ hydra.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
 
-PROJECT_TITLE=hydra
-ABBREVIATION=hydra
-VERSION=0.0.4-test
+VERSION="0.0.4-test"
 
 HOME_DIR=$(dirname $0)
 
-if [ $# -ge 1 ]; then
-    OPTION=$1
+APACHE_MAVEN_KEYWORD="apache-maven"
 
-    if [ $OPTION = version ]; then
-        echo $VERSION
-        exit 0
+apache-maven-init() {
+    PROJECT_NAME="$1"
+
+    VALID_PROJECT_NAME="^[a-zA-Z0-9_\-\.]+$"
+
+    if [[ ! "$PROJECT_NAME" =~ $VALID_PROJECT_NAME ]]; then
+        echo "Invalid input: \"$PROJECT_NAME\""
+        exit 1
     fi
 
-    if [ $OPTION = apache-maven ]; then
-        shift 1
-        source $HOME_DIR/scripts/apache-maven.sh $@
-        exit 0
-    fi
-fi
+    cp -r "$HOME_DIR/resources/$APACHE_MAVEN_KEYWORD/default-template" "$PROJECT_NAME"
+}
 
-cat <<EOF
-hydra [ OPTIONS ]
-================================================<[ OPTIONS ]>================================================
-> help           > Shows this options menu
-> version        > Shows the current version of hydra
-> apache-maven   > Generate apache-maven templates
-EOF
+apache-maven() {
+    if [ $# -ge 1 ]; then
+        OPTION="$1"
+
+        if [ "$OPTION" = "init" ]; then
+            shift 1
+            apache-maven-init $@
+            exit 0
+        fi
+    fi
+
+    echo "hydra $APACHE_MAVEN_KEYWORD [ OPTIONS ]"
+    echo "================================================<[ OPTIONS ]>================================================"
+    echo "> init [ PROJECT_NAME ] ..... > Generate new default $APACHE_MAVEN_KEYWORD template"
+}
+
+hydra() {
+    if [ $# -ge 1 ]; then
+        OPTION="$1"
+
+        if [ "$OPTION" = "version" ]; then
+            echo "$VERSION"
+            exit 0
+        fi
+
+        if [ "$OPTION" = "$APACHE_MAVEN_KEYWORD" ]; then
+            shift 1
+            apache-maven $@
+            exit 0
+        fi
+    fi
+
+
+    echo "hydra [ OPTIONS ]"
+    echo "================================================<[ OPTIONS ]>================================================"
+    echo "> help ..... > Shows this options menu"
+    echo "> version ..... > Shows the current version of hydra"
+    echo "> $APACHE_MAVEN_KEYWORD ..... > Generate $APACHE_MAVEN_KEYWORD templates"
+}
+
+hydra $@
--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-PROJECT_TITLE=hydra
-ABBREVIATION=hydra
 VERSION=0.0.4-test
 
-if [ -d ~/.$PROJECT_TITLE ]; then
-    rm -fr ~/.$PROJECT_TITLE >/dev/null 2>/dev/null
+if [ -d "~/.$PROJECT_TITLE" ]; then
+    rm -fr "~/.$PROJECT_TITLE" >/dev/null 2>/dev/null
     
     if [[ $? -ne 0 ]]; then
         echo "[ ERROR ] Failed to delete folder: \"~/.$PROJECT_TITLE\""
@@ -15,7 +13,7 @@ fi
 
 echo "................................................................ (01 / 04)"
 
-mkdir ~/.$PROJECT_TITLE >/dev/null 2>/dev/null
+mkdir "~/.$PROJECT_TITLE" >/dev/null 2>/dev/null
 
 if [[ $? -ne 0 ]]; then
     echo "[ ERROR ] Failed to create folder: \"~/.$PROJECT_TITLE\""
@@ -24,7 +22,7 @@ fi
 
 echo "................................................................ (02 / 04)"
 
-cd ~/.$PROJECT_TITLE >/dev/null 2>/dev/null
+cd "~/.$PROJECT_TITLE" >/dev/null 2>/dev/null
 
 if [[ $? -ne 0 ]]; then
     echo "[ ERROR ] Failed to change directory: \"~/.$PROJECT_TITLE\""
@@ -33,7 +31,7 @@ fi
 
 echo "................................................................ (03 / 04)"
 
-git clone git@github.com:AverageOrangeCat/$PROJECT_TITLE.git . --branch $VERSION >/dev/null 2>/dev/null
+git clone "git@github.com:AverageOrangeCat/$PROJECT_TITLE.git" . --branch "$VERSION" >/dev/null 2>/dev/null
 
 if [[ $? -ne 0 ]]; then
     echo "[ ERROR ] Failed to clone the repository"
